d954f4306a3f76fc9035fee6bcdbf17e8134ae19
Abhix <abhinabasen2022@gmail.com>
2023-03-31 22:14:32 +0530

    commited

--- medcity/book/succes.js
+++ medcity/book/succes.js
@@ -1,7 +1,14 @@
 var urlParams = new URLSearchParams(window.location.search);
-var token = urlParams.get("mqfurfeg");
-  document.querySelector(".num").innerHTML = token;
 
+if (urlParams.has('mqfu48yrejirehreurerguyr8733r8783r38ri3vru836fruyhjefryerrerererr3hry98rurfeg')){
+
+  var token = urlParams.get("mqfu48yrejirehreurerguyr8733r8783r38ri3vru836fruyhjefryerrerererr3hry98rurfeg");
+    document.querySelector(".num").innerHTML = token;
+
+}  
+else {
+window.location = "index.html";
+}
 
 function goback(){
   history.back();
